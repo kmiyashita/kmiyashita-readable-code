40d3e6f75d95709549434a651a4e3c7fed7df6fd
kmiyashita <kotaro.miyashita@miraclelinux.com>
2015-06-24 11:47:59 +0900

    オムライスをレシピのタイトルとして出力するようにした。

--- omurice.c
+++ omurice.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
+/*仕様1の実装*/
+
+/*RECIPE構造体の定義*/
+struct RECIPE{
+             char *title;
+             };
+
 int main() {
-        printf("omurice\n");
+struct RECIPE recipe;
+
+recipe.title="オムライス"; /*タイトルにオムライスを代入する*/
+        printf("タイトル:「%s」\n",recipe.title); /*"タイトル"を標準出力に出力する*/
         return 0;
 }
